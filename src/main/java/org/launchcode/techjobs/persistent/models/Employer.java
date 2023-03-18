367e9d21026eb1da34278922442d57961a769e2f
djh007ktr <djhill0531@gmail.com>
2023-03-18 16:14:51 -0500

    Pt2Models

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.*;;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotEmpty
+    @Size(min=3, max=30, message="Must be between 3 and 30 characters")
+    private String location;
 
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,19 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Min(value = 100, message = "Short description under 100 characteres")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
