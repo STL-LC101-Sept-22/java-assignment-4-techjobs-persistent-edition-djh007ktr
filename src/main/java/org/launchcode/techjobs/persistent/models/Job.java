1dbccadfc608121b0b6e855cccded2731389815f
djh007ktr <djhill0531@gmail.com>
2023-03-19 17:53:18 -0500

    Pt4ListController

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -9,7 +9,7 @@ public class Job extends AbstractEntity{
 
     @ManyToOne
     private Employer employer;
-    @ManyToMany(mappedBy = "jobs")
+    @ManyToMany
     private List<Skill> skills = new ArrayList<>();
 
     public Job() {
