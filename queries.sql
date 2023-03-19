4b0e2ad5ee650c989469af8a038c63d6885e5cb9
djh007ktr <djhill0531@gmail.com>
2023-03-19 18:48:17 -0500

    Pt4SqlFin_FIN

--- queries.sql
+++ queries.sql
@@ -18,3 +18,8 @@ DROP TABLE job;
 
 
 -- Part 4: Test it with SQL
+SELECT *
+FROM skill
+INNER JOIN job_skills ON job_skills.skills_id = skill.id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
