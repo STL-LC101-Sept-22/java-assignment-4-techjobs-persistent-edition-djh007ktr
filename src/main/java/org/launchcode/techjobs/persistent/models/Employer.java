827e7503b7c3623255e3f0970272ddff2e6e5e90
djh007ktr <djhill0531@gmail.com>
2023-03-18 19:05:41 -0500

    Pt3_1toManyEmployerToJobsModels

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,7 +1,10 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.*;
-import javax.validation.constraints.*;;
+import javax.validation.constraints.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -9,6 +12,10 @@ public class Employer extends AbstractEntity {
     @NotNull
     @Size(min=3, max=30, message="Must be between 3 and 30 characters")
     private String location;
+
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer(){}
 
